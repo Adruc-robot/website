61e8f1795f4f8ca77673c828233760898079d2b9
Daniel Curda <dgcurda@gmail.com>
2026-06-24 08:36:21 -0600

    updated get for calendar to remove app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
     activePage: "calendar",
   });
 });*/
-app.get("/app.js/calendar", (req, res) => {
+app.get("/calendar", (req, res) => {
   /*res.render("calendar/index", {
     title: "Calendar",
     activePage: "calendar",
